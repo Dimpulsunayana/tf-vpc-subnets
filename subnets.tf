57b61eadf8fd764ed3b7e0970a063cffc6752446
windows 10 <dimpulsunayana205@gmail.com>
2023-03-30 20:35:03 +0530

    updated user repo

--- subnets.tf
+++ subnets.tf
@@ -4,8 +4,7 @@ module "subnets" {
 
   env =var.env
   main_vpc = aws_vpc.main.id
-  vpc_id = aws_vpc.main.id
-  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
+  peering_id = aws_vpc_peering_connection.vpc_peering.id
   #peering_id = var.peering_id
   for_each    = var.public_subnets
   default_vpc_id = var.default_vpc_id
