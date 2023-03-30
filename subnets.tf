5f64f1173a8c1fbe36c20b55630e88153226f99b
windows 10 <dimpulsunayana205@gmail.com>
2023-03-30 13:35:48 +0530

    updated user repo

--- subnets.tf
+++ subnets.tf
@@ -3,4 +3,10 @@ module "subnets" {
 
   env =var.env
   main_vpc = aws_vpc.main.id
+
+  for_each = var.subnets
+  cidr_block = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+  name = each.value.name
+
 }
