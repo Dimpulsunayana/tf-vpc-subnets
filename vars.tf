7fbbb4a0451d350d2a6581e4c0bc4658077f6683
windows 10 <dimpulsunayana205@gmail.com>
2023-03-30 17:59:57 +0530

    updated user repo

--- vars.tf
+++ vars.tf
@@ -3,5 +3,5 @@ variable "cidr_block" {}
 variable "default_vpc_id" {}
 #variable "private_subnets" {}
 variable "availability_zone" {}
-variable "subnet_ids" {}
+#variable "subnet_ids" {}
 variable "public_subnets" {}
