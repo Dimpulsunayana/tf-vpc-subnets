be3e49c1474c488f46a701a0e9200de56ec2f394
windows 10 <dimpulsunayana205@gmail.com>
2023-03-30 14:33:53 +0530

    updated user repo

--- subnets.tf
+++ subnets.tf
@@ -11,21 +11,3 @@ module "subnets" {
   availability_zone = each.value.availability_zone
   name = each.value.name
 }
-
-variable "subnets" {
-    public_subnets = {
-      public = {
-        name              = "public"
-        cidr_block        = ["10.0.0.0/24", "10.0.1.0/24"]
-        availability_zone = ["us-east1a", "us-east1b"]
-      }
-    }
-
-    private_subnets = {
-      private = {
-        name              = "private"
-        cidr_block        = ["10.0.2.0/24", "10.0.3.0/24"]
-        availability_zone = ["us-east1a", "us-east1b"]
-      }
-    }
-  }
--- vars.tf
+++ vars.tf
@@ -1,7 +1,7 @@
 variable "env" {}
 variable "cidr_block" {}
 variable "default_vpc_id" {}
-#variable "subnets" {}
+variable "subnets" {}
 #variable "private_subnets" {}
 variable "availability_zone" {}
 variable "name" {}
