4eb303336aa07237c56b6af27f9806c3f1f230f9
windows 10 <dimpulsunayana205@gmail.com>
2023-04-12 19:39:35 +0530

    updated user repo

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "main_vpc" {
 output "peering_id" {
   value = aws_vpc_peering_connection.vpc_peering.id
 }
+
+output "public_subnet_ids" {
+  value = module.public_subnets
+}
+
+output "private_subnet_ids" {
+  value = module.private_subnets
+}
